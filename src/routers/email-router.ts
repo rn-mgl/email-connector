d0f6aa3732ee7d16c631ca519476f0c57784e2b2
rn-mgl <reinb2ra@gmail.com>
2025-12-02 20:14:15 +0800

    add default

--- src/routers/email-router.ts
+++ src/routers/email-router.ts
@@ -3,6 +3,9 @@ import express from "express";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.json({ message: "test" });
+});
 router.post("/", handleEmail);
 
 export default router;
